fabe828331adad8d04d7b2fc09a1358b7eb58c23
Gaga-Demetrashvili <gagademetrashvili6@gmail.com>
2024-01-08 20:44:51 +0400

    Add employee creation logic

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -19,5 +19,7 @@ public class MappingProfile : Profile
         CreateMap<CompanyForCreationDto, Company>();
 
         CreateMap<Employee, EmployeeDto>();
+
+        CreateMap<EmployeeForCreationDto, Employee>();
     }
 }
